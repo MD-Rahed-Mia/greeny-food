cc37893da65dc25f3102e30e68ba4020a611f4b2
mdrahed2023 <mdrahed708@gmail.com>
2023-08-22 19:18:14 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+
+
+const addBtn = document.querySelectorAll(".soldItem button");
+const soldItem = document.querySelectorAll(".soldItem img");
+
+
+let count = 1;
+
+addBtn.forEach((e) => {
+
+  e.addEventListener("click", () => {
+
+    e.innerHTML = ""
+
+    let minusBtn = document.createElement("button");
+    let plusBtn = document.createElement("button");
+    let result = document.createElement("button");
+
+    minusBtn.classList.add("addBtn");
+    result.classList.add("addBtn");
+    plusBtn.classList.add("addBtn");
+
+    minusBtn.innerHTML = "-";
+    plusBtn.innerHTML = "+";
+    result.innerHTML = count;
+
+
+    minusBtn.addEventListener("click", () => {
+      count--;
+    })
+
+    plusBtn.addEventListener("click", () => {
+      count++;
+    })
+
+    e.appendChild(minusBtn);
+    e.appendChild(result);
+    e.appendChild(plusBtn);
+
+    
+    if (count < 1) {
+      minusBtn.disabled = true;
+    }
+
+
+  })
+
+})
+
+
+soldItem.forEach((e) => {
+  e.addEventListener("click", () => {
+    location.href = "product.html";
+  })
+})
+
